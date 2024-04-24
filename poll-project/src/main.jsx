67f17f30c084759d5ee92eb97567dbf9a18b1022
amdequit <80928836+amdequit@users.noreply.github.com>
2024-04-24 11:33:06 -0700

    update main to show text

--- poll-project/src/main.jsx
+++ poll-project/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createRoot } from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <h1>hello world</h1>
   </React.StrictMode>,
 )
