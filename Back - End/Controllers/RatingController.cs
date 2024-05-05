733cdf538dd4734e81437d543fb6dbfb53c9e9b2
JesusFranC <94206855+JesusFranC@users.noreply.github.com>
2024-05-05 12:01:55 -0700

    Deleting obj

---
+++ Back - End/Controllers/RatingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Team3.ThePollProject.Controllers
+{
+    public class RatingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
