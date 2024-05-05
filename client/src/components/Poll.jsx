8362974c541aa0e0da54218421a56d328f089011
amdequit <80928836+amdequit@users.noreply.github.com>
2024-05-04 21:21:25 -0700

    update Poll.jsx props, body

--- client/src/components/Poll.jsx
+++ client/src/components/Poll.jsx
@@ -2,9 +2,25 @@ import React, { useState, useEffect }  from 'react'
 
 //USE CASE #4: I want to view reviews and ratings for different campus entities, so
 //I can make informed decisions about my campus experiences.
-export const Poll = () => {
+export const Poll = ({ title, description, maxVotes, pollOptions = [] }) => {
+  title = 'What is your favorite food?'
+  description = 'We are trying to decide what to serve at the next event.'
+  maxVotes = 4
+
+  pollOptions = ['Pizza', 'Tacos', 'Burgers', 'Salad']
   return (
-    <div>this is a poll</div>
+    <div className="font-sans flex justify-start bg-lbsu-yellow max-w-[240px] max-h-[135px]">
+      <h3 className='text-xl font-bold'>{title}</h3>
+      <p className='text-sm'>{description}</p>
+      <p className='text-sm'>Max Votes: {maxVotes}</p>
+      {pollOptions.map((option, i) => (
+        <div key={i} className='flex items-center'>
+          <input type='checkbox' />
+          <label>{option}</label>
+        </div>
+      ))}
+    </div>
+    
   )
 }
 
