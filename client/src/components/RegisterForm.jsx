bac5d4d976ba26a1b1670e3ca80a3872b76065de
amdequit <80928836+amdequit@users.noreply.github.com>
2024-05-06 18:44:42 -0700

    Add password validation, hide/show password to RegisterForm.jsx

--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -7,6 +7,8 @@ export const RegisterForm = () => {
     //declare variables for form fields
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');   
+    const [password2, setPassword2] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
     
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -17,10 +19,14 @@ export const RegisterForm = () => {
         if (username === '' || password === '') {
             alert('Please fill out all fields');
         }
+        if (password !== password2) {
+            alert('Passwords do not match');
+        }
     }
+
   return (
-    <div className="font-sans w-[520px] h-[586px] bg-light-grey-1 rounded-2xl">
-    <div className="flex flex-col pt-20 px-12">
+    <div className="font-sans min-w-[520px] h-full min-h-[586px] bg-light-grey-1 rounded-2xl">
+    <div className="flex flex-col items-center pt-20 px-12">
       <h3 className='text-3xl font-semibold pb-4'>Register for Rating and Polling</h3>
       <form className='justify-start py-3' onSubmit={handleSubmit}> {/* form*/}
         <div className="pb-2"> {/* field*/}
@@ -32,32 +38,43 @@ export const RegisterForm = () => {
             />
         </div>
         <div className='pb-2'>
-            <input type="text"
+            <input 
+                type={
+                    showPassword ? "text" : "password"
+                }
                 name='password'
                 placeholder="Enter your password"
+                value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 className="flex items-center m-auto w-[310px] h-[50px] bg-[#eaeaea] rounded-md p-[14px] focus:outline-none"
             />
         </div>
         <div className='pb-2'>
-            <input type="text"
+            <input 
+                type={
+                    showPassword ? "text" : "password"
+                }
                 name='password'
-                placeholder="Enter your password again"
-                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Confirm your password"
+                onChange={(e) => setPassword2(e.target.value)}
                 className="flex items-center m-auto w-[310px] h-[50px] bg-[#eaeaea] rounded-md p-[14px] focus:outline-none"
             />
         </div>
-        <div className='flex flex-col items-center justify-center m-2 overflow-auto'>
+        <input 
+            type="checkbox" 
+            id="check" 
+            onClick={() => setShowPassword((prev) => !prev)} />
+        <label className='px-1' for="check">Show Password</label>
+        
+        <div className='flex flex-col items-center justify-center m-2 overflow-auto py-2'>
             <button type="submit" onClick={() => {validateForm()}}
                 className="btn-register">
                 Register
             </button>
-            <a className='text-sm py-4' href='/'>Already have an account? Login</a>
+            <a className='text-sm py-4 italic underline' href='/'>Already have an account? Login</a>
         </div>
         
       </form>
-      
-      <div className='w-16 h-1.5 mt-3 bg-lbsu-blue rounded-lg'></div>
     </div>
   </div>
   )
