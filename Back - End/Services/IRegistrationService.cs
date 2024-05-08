2e845e2c3c381676417f9f279871966ff79ebd2c
jason barber <jasonthegamer84@gmail.com>
2024-05-07 22:11:07 -0700

    eds

---
+++ Back - End/Services/IRegistrationService.cs
@@ -0,0 +1,10 @@
+﻿using Team3.ThePollProject.Models.Response;
+
+namespace Team3.ThePollProject.Services
+{
+    public interface IRegistrationService
+    {
+        bool DoesEmailExist(string email);
+        IResponse MakeUser(string email);
+    }
+}
