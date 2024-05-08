8f0c49127e413d015e255124cf799690bb7f43db
amdequit <80928836+amdequit@users.noreply.github.com>
2024-05-07 23:12:49 -0700

    remove maxVotes from Poll.jsx

--- client/src/components/Poll.jsx
+++ client/src/components/Poll.jsx
@@ -1,45 +1,37 @@
-import React, { useState, useEffect, useContext }  from 'react'
-import { AuthContext } from '../context/AuthProvider';
+import React, { useState, useEffect, useContext }  from 'react';
+// import { AuthContext } from '../context/AuthProvider';
 
 //USE CASE #4: I want to view reviews and ratings for different campus entities, so
 //I can make informed decisions about my campus experiences.
-export const Poll = ({ Title, description, pollOptions = [] }) => {
+export const Poll = ({ title, description, pollOptions = [] }) => {
   //for pollingModel
   //FIXME: To-DO: 
   //1. persistence = get user from context
   //2. handleVote = send poll response to backend
   //3. button - mark as selected
   //4. Limit vote to 1 per user
-  const {authUser, setAuthUser} = useContext(AuthContext);
+  // const {authUser, setAuthUser} = useContext(AuthContext);
   const [user, setUserDetails] = useState(null);
   const createdAt = new Date();
 
   //FIXME: hardcoded values for now
-  Title = 'Best convenience store?'
+  title = 'Best convenience store?'
   description = 'just curious wat u guys think?? evaluating based on their snack options and how stocked they are.'
-  maxVotes = 10
   pollOptions = ['Outpost Convenience Store', 'Bookstore Convenience Store', 'Caffiene Lab', 'WallStrEAT Cafe']
-  
-  const handleVote = (e) => {
-    e.preventDefault();
-    console.log('voted: ', e.target.textContent);
-    //TODO: send poll response to backend
-  }
-
 
   return (
     <div className="font-sans w-[520px] h-[586px] bg-light-grey-1 rounded-2xl">
       <div className="flex flex-col pt-20 px-12">
-        <h3 className='text-3xl font-semibold pb-4'>{Title}</h3>
+        <h3 className='text-3xl font-semibold pb-4'>{title}</h3>
         <p className='text-sm py-4'>{description}</p>
-        <form className='flex flex-col' onSubmit={handleVote}>
+        {/* <form className='flex flex-col' onSubmit={handleVote}> */}
+        <form className='flex flex-col'>
           {pollOptions.map((option, i) => (
             <button key={i} className='btn-poll' type='submit'>
               <label className='px-3'>{option}</label>
             </button>
           ))}
         </form>
-        <p className='text-sm py-4'>Max Votes: {maxVotes}</p>
         <div className='w-16 h-1.5 mt-3 bg-lbsu-blue rounded-lg'></div>
         <p className='text-sm py-3 italic'>Posted on {createdAt.toLocaleString()}</p>
       </div>
