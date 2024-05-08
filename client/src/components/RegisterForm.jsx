76c9570aa0fa8305f1f04093856ac6439d1fbe06
amdequit <80928836+amdequit@users.noreply.github.com>
2024-05-08 10:49:21 -0700

    update register form

--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -5,7 +5,7 @@ import React, { useState, useEffect }  from 'react'
 // someone else’s actions, so I can ensure the security of my account.
 export const RegisterForm = () => {
     //declare variables for form fields
-    const [email, setEmail] = useState(''); //FIXME: Change to [username, setUsername]
+    const [email, setEmail] = useState(''); 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');   
     const [password2, setPassword2] = useState('');
@@ -17,38 +17,23 @@ export const RegisterForm = () => {
         //Implement submitting form data to the server
         const url = 'http://localhost:5206/Registeration/Register';
         try {
-            if (validateForm()) {
-                console.log('Form is valid');
-                const response = await fetch (url, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(email) //FIXME: add username, password
-                });
-                if (response.ok) {
-                    console.log('User registered', {email});
-                    e.target.reset(); 
-                } else {
-                    alert('Registration failed. Please try again.');
-                }
+            const response = await fetch (url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(email) //FIXME: add username, password
+            });
+            if (response.ok) {
+                console.log('User registered', {email});
+                e.target.reset(); 
+            } else {
+                alert('Registration failed. Please try again.');
             }
         } catch (error) {
             console.log('Error in registration: ', error);
         }
-        console.log('submitted: ', email, username, password);
-    }
-
-    const validateForm = () => {
-        if (username === '' || password === '') {
-            alert('Please fill out all fields');
-            return false;
-        }
-        if (password !== password2) {
-            alert('Passwords do not match');
-            return false;
-        }
-        return true;
+        console.log('submitted: ', email);
     }
 
   return (
@@ -64,45 +49,9 @@ export const RegisterForm = () => {
             className="field-input"
             />
         </div>
-        <div className="pb-2"> {/* field*/}
-            <input type="text"
-            name='username'
-            placeholder="Enter your username"
-            onChange={(e) => setUsername(e.target.value)}
-            className="field-input"
-            />
-        </div>
-        <div className='pb-2'>
-            <input 
-                type={
-                    showPassword ? "text" : "password"
-                }
-                name='password'
-                placeholder="Enter your password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="field-input"
-            />
-        </div>
-        <div className='pb-4'>
-            <input 
-                type={
-                    showPassword ? "text" : "password"
-                }
-                name='password'
-                placeholder="Confirm your password"
-                onChange={(e) => setPassword2(e.target.value)}
-                className="field-input"
-            />
-        </div>
-        <input 
-            type="checkbox" 
-            id="check" 
-            onClick={() => setShowPassword((prev) => !prev)} />
-        <label className='px-2' for="check">Show Password</label>
         
         <div className='flex flex-col items-center justify-center m-2 overflow-auto py-4'>
-            <button type="submit" onClick={() => {validateForm()}}
+            <button type="submit"
                 className="btn-register">
                 Register
             </button>
